d675cbae940734a55bb581b480bfc93627780bc9
StyvekDrtic <jiristepanek@hotmail.com>
2021-08-25 17:13:08 +0200

    init

---
+++ _site/assets/js/app.js
@@ -0,0 +1,79 @@
+$('.picture').each( function() {
+    var $pic     = $(this),
+        getItems = function() {
+            var items = [];
+            $pic.find('a').each(function() {
+                var $href   = $(this).attr('href'),
+                    $size   = $(this).data('size').split('x'),
+                    $width  = $size[0],
+                    $height = $size[1];
+
+                var item = {
+                    src : $href,
+                    w   : $width,
+                    h   : $height
+                }
+
+                items.push(item);
+            });
+            return items;
+        }
+
+    var items = getItems();
+
+    var $pswp = $('.pswp')[0];
+    $pic.on('click', 'figure', function(event) {
+        event.preventDefault();
+
+        var $index = $(this).index();
+        var options = {
+            index: $index,
+            bgOpacity: 0.7,
+            showHideOpacity: true
+        }
+
+        // Initialize PhotoSwipe
+        var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+        lightBox.init();
+    });
+});
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+$(".dolniHeader").animate({ scrollLeft: "100px" }, 800, "swing");
+$(".dolniHeader").animate({ scrollLeft: "0px" }, 800, "swing");
